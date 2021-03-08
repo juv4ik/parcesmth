5e180a04ab851b3e086c6055a716b9d1c6dd8abe
juv4ik <juv4ik@gmail.com>
2021-03-09 00:13:51 +0300

    add url, get response status_code, response.text got goods and price without empty symbols export to outdata.txt

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import requests
 from lxml import html
 
 def cnnct(url):
-    #url = 'https://diy.by/catalog/instrument_and_electrical/976.html?PAGECOUNT=80&FIRSTLEVID=instrument_and_electrical&CATSECT=976&PAGEN_1=1'
     response = requests.get(url = url)
     print(response.status_code)
     tree = html.fromstring(response.text)
-    #print(response.text)
     return tree
 
 def get_data(tree):
@@ -24,7 +22,6 @@ def get_data(tree):
         temp2_conv_text = temp_conv_text.replace("\n", "")
         conv_name = temp2_conv_text.replace("                                            ", "")
         conv_price = text2.replace("                                   ", ":")
-        #print(conv_name + ":" + conv_price)
         a.append(conv_name + ":" + conv_price)
     return a
 
