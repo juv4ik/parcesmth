d6c2660b245ac0b539a5b67645e72aed9a9d972c
juv4ik <juv4ik@gmail.com>
2021-03-08 23:03:48 +0300

    add url, get response status_code, response.text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # парсинг diy by
+import requests
+from lxml import html
+
+url = 'https://diy.by/catalog/instrument_and_electrical/976.html?PAGECOUNT=80&FIRSTLEVID=instrument_and_electrical&CATSECT=976&PAGEN_1=1'
+response = requests.get(url = url)
+print(response.status_code)
+tree = html.fromstring(response.text)
+print(response.text)
