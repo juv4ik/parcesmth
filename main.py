18b280a1e4fa6de02ce09abdf1691ff755552799
juv4ik <juv4ik@gmail.com>
2021-03-08 23:27:29 +0300

    add url, get response status_code, response.text got goods and price

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ tree = html.fromstring(response.text)
 #print(response.text)
 
 xpath = f"//div/div[@class='td_name' and 3]"
+xpath2 = f"//div[@class='td_price']"
 element = tree.xpath(xpath)
+price = tree.xpath(xpath2)
 print(len(element))
 for i in range(len(element)):
     text = element[i].text
-    print(i, text)
+    text2 = price[i].text
+    temp_conv_text = text.replace("                                                    ", "")
+    conv_text = temp_conv_text.replace("                                            ", "")
+    conv_text2 = text2.replace("                                   ", ":")
+    print(conv_text + ":" + conv_text2)
+    #print(conv_text2)
